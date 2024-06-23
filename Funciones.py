cda5d6a5f4839dde9bc7fb28fbac1ae101e238c1
Benjamin <ben.arayag@duocuc.cl>
2024-06-22 21:12:07 -0400

    Opcion 1 finalizada

--- Funciones.py
+++ Funciones.py
@@ -1,6 +1,19 @@
 #Funciones Biblioteca
+import time,os,msvcrt
+import json
+
+libreria=[]
 def opc1():
-    pass
+    print('Añadir libro: ')
+    titulo=input('Ingresa el titulo de el libro: ')
+    anio_publicacion=int(input('Ingrese el año de publicación: '))
+    gen_libro=input('Ingrese genero de el libro: ')
+    libros={'nombre':titulo,
+            'año':anio_publicacion,
+            'genero':gen_libro}}
+    libreria.append(libros)
+    time.sleep(2)
+    print('LIBRO AGREGADO CON ÉXITO...')
 def opc2():
     pass
 def opc3():
